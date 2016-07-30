7d22b698f1d4f5c2b5d941d2ac12c7030cba1d93
Didier JOOMUN <d.joomun@linkbynet.com>
2016-07-30 19:29:03 +0700

    Testing basic movements

--- robot1/robot1.ino
+++ robot1/robot1.ino
@@ -13,28 +13,50 @@ int buzzPin =  2;    //Connect Buzzer on Digital Pin2
 void setup()   
 {
   servo1.attach(8);
-  servo1.attach(9);
+  servo2.attach(9);
   pinMode(buzzPin, OUTPUT);      
 } 
 
+/* Defining robots functions
+ * 
+ * 
+ */
+ 
 void move_fwd(){
- servo1.write(0);  
+ servo1.write(180);
+ servo2.write(0);  
 }
 
 void move_bkwd(){
-  
+ servo1.write(0);
+ servo2.write(180);   
 }
 
 void rotate_left(){
-  
+ servo1.write(180);
+ servo2.write(180);   
 }
 
 void rotate_right(){
-  
+ servo1.write(0);
+ servo2.write(0);     
+}
+void brake(){
+ servo1.write(90);
+ servo2.write(90);     
 }
 
 
- void loop()                      
+void loop()                      
 { 
-  move_fwd();  
+  int DLY = 3000;
+  
+  move_fwd();
+  delay(DLY); 
+  brake();
+  delay(DLY); 
+  rotate_left(); 
+  delay(DLY);
+  rotate_right(); 
+  delay(DLY); 
 }
