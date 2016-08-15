b38ef152ff13f550d963c37ebfad38702ddf13e2
Michel Didier Joomun <didier@joomun.org>
2016-08-15 09:36:44 +0700

    Update robot1.ino
    Adding Ultrasound module and obstacle avoidance logic

--- robot1/robot1.ino
+++ robot1/robot1.ino
@@ -10,17 +10,25 @@ Servo servo1;
 Servo servo2;
 int buzzPin =  2;    //Connect Buzzer on Digital Pin2 
 
+const int trigPin = 10;
+const int echoPin = 11;
+
+long duration;
+int distance;
+
 void setup()   
 {
   servo1.attach(8);
   servo2.attach(9);
-  pinMode(buzzPin, OUTPUT);      
+  pinMode(buzzPin, OUTPUT);  
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(9600);  
 } 
 
 /* Defining robots functions
  * 
- * 
- */
+*/
  
 void move_fwd(){
  servo1.write(180);
@@ -46,9 +54,31 @@ void brake(){
  servo2.write(90);     
 }
 
+void loop() { 
 
-void loop()                      
-{ 
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+
+if (distance < 10) {
+  tone(buzzPin,400,1500);
+  rotate_left();
+} else {
+  move_fwd();
+}
+delay(500);
+  /*
   int DLY = 3000;
   
   move_fwd();
@@ -59,4 +89,5 @@ void loop()
   delay(DLY);
   rotate_right(); 
   delay(DLY); 
+*/
 }
